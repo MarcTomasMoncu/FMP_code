a64bde80609740e5ceb936d72ff0e7880cded5fe
MarcTomasMoncu <marctommon@gmail.com>
2026-05-06 11:58:46 +0200

    intnt de ocrreccio del gtafic

--- interpretability.py
+++ interpretability.py
@@ -14,7 +14,7 @@ def generate_shap_summary(model, X_test, model_name, output_path):
             shap_values = shap_values[1]
             
         plt.figure()
-        shap.summary_plot(shap_values, X_test, show=False)
+        shap.summary_plot(shap_values, X_test, max_display=20, show=False) 
         plt.savefig(os.path.join(output_path, f"{model_name}_shap_summary.png"), bbox_inches='tight')
         plt.close()
         print(f"Grafic SHAP generat per a {model_name}.")
